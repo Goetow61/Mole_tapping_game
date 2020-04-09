9ef0442dadcbe7147191f108502d6634d4a7d18e
Gvirus_ <u0ku0su0tu0nu@gmail.com>
2020-04-09 20:37:44 +0900

    制限時間をカウントダウンする。 Timeの位置を微調整。 テキストを「残り 0.0f」にする。

---
+++ Assets/Scripts/TimerController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimerController : MonoBehaviour
+{
+    float countTime = 15.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (countTime > 0.0f)
+        {
+            countTime -= Time.deltaTime; // 残り秒数を格納
+            GetComponent<Text>().text = string.Format("残り {0:F1}", countTime); // 残り秒数を表示
+        }
+        else
+        {
+            // 時間切れ時の処理
+        }
+    }
+}
