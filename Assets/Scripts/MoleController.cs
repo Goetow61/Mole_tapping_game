b129ccb16364b11fc886909cf36ff5e15ff89769
Gvirus_ <u0ku0su0tu0nu@gmail.com>
2020-04-10 04:44:03 +0900

    MoleControllerの作成。Mole作成時にアタッチさせる必要がある。

---
+++ Assets/Scripts/MoleController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoleController : MonoBehaviour
+{
+    public 
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Debug.Log(countTime);
+    }
+
+    // モグラをクリックしたら、モグラを消す
+    public void MoleClick()
+    {
+        Destroy(gameObject);
+    }
+}
