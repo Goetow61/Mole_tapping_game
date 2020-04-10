09db4629e10234f0d53df392ddfb138bbc51ee29
Gvirus_ <u0ku0su0tu0nu@gmail.com>
2020-04-10 13:02:36 +0900

    MoleControllerのコーディング。 MoleのGenerate時にアタッチさせる必要がある

--- Assets/Scripts/MoleController.cs
+++ Assets/Scripts/MoleController.cs
@@ -1,14 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MoleController : MonoBehaviour
 {
-    public 
+    private Button moleButton;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        // 自分自身(Mole)のButtonコンポーネントを取得
+        moleButton = GetComponent<Button>();
+
+        // モグラを消すメソッドをonClickイベントに紐づける
+        moleButton.onClick.AddListener(MoleClick);
     }
 
     // Update is called once per frame
