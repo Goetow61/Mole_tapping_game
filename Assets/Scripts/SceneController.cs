d5d349e2257a29b35a11e0f28807e32ea5bb5159
Gvirus_ <u0ku0su0tu0nu@gmail.com>
2020-04-12 03:59:43 +0900

    Topシーンの、2つのボタンに音を設定

--- Assets/Scripts/SceneController.cs
+++ Assets/Scripts/SceneController.cs
@@ -5,6 +5,9 @@ using UnityEngine.SceneManagement;
 
 public class SceneController : MonoBehaviour
 {
+    public AudioClip button;
+    public AudioClip startbutton;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -20,18 +23,21 @@ public class SceneController : MonoBehaviour
     // ゲームを始めるボタンを押したらゲームシーンに切り替え
     public void StartButtonClick()
     {
+        AudioSource.PlayClipAtPoint(startbutton, new Vector3(0, 0, 0));
         SceneManager.LoadScene("Main");
     }
 
     // 履歴を見るボタンを押したら履歴シーンに切り替え
     public void HistoryButtonClick()
     {
+        AudioSource.PlayClipAtPoint(button, new Vector3(0, 0, 0));
         SceneManager.LoadScene("History");
     }
 
     // ホームボタンを押したらTopシーンに切り替え
     public void HomeButtonClick()
     {
+        AudioSource.PlayClipAtPoint(button, new Vector3(0, 0, 0));
         SceneManager.LoadScene("Top");
     }
 }
