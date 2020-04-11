09a70c23743b9fb3d986cd13fb7e9423595db095
Gvirus_ <u0ku0su0tu0nu@gmail.com>
2020-04-11 14:29:07 +0900

    プレイ結果をCSVファイルに保存

--- Assets/Scripts/ScoreController.cs
+++ Assets/Scripts/ScoreController.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 using System;
+using System.Text;
+using System.IO;
 
 public class ScoreController : MonoBehaviour
 {
@@ -28,5 +30,20 @@ public class ScoreController : MonoBehaviour
     public void SetDateTime()
     {
         date1 = DateTime.Now;
+        SaveScore();
+    }
+
+    void SaveScore()
+    {
+        // ファイル書き出し
+        // 現在のフォルダにsaveData.csvを出力する(決まった場所に出力したい場合は絶対パスを指定してください)
+        // 引数説明：第1引数→ファイル出力先, 第2引数→ファイルに追記(true)or上書き(false), 第3引数→エンコード
+        StreamWriter sw = new StreamWriter(@"SaveScore.csv", true, Encoding.GetEncoding("Shift_JIS"));
+        // データ出力
+        string[] str = { date1.ToString("yyyy/MM/dd HH:mm"), score.ToString() };
+        string str2 = string.Join(",", str);
+        sw.WriteLine(str2);
+        // StreamWriterを閉じる
+        sw.Close();
     }
 }
