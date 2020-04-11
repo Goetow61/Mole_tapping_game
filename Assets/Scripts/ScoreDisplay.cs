bd0e37411e817c79a332ee4c4dd4016c3faf6ae3
Gvirus_ <u0ku0su0tu0nu@gmail.com>
2020-04-11 14:00:24 +0900

    カウントしておいたスコアを表示

---
+++ Assets/Scripts/ScoreDisplay.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreDisplay : MonoBehaviour
+{
+    public GameObject ScoreController;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        int score = ScoreController.GetComponent<ScoreController>().score;
+        GetComponent<Text>().text = score.ToString("000") + "点";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
