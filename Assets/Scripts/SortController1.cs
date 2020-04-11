d04fce74c4588fcc2cdbbcde26fe0114e1b2d190
Gvirus_ <u0ku0su0tu0nu@gmail.com>
2020-04-12 02:38:30 +0900

    日時ヘッダーをクリックして、日時ソートを昇順降順切り替える。

---
+++ Assets/Scripts/SortController1.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SortController1 : MonoBehaviour
+{
+    public GameObject HistoryGenerator;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // 日時ヘッダをクリックしたら、日時ソートする
+    public void HeaderDateTimeClick()
+    {
+        if ( this.tag == "Descending" )
+        {
+            this.tag = "Ascending";
+            HistoryGenerator.GetComponent<HistoryGenerator>().SortAndGenerate(false, 0);
+        }
+        else
+        {
+            this.tag = "Descending";
+            HistoryGenerator.GetComponent<HistoryGenerator>().SortAndGenerate(true, 0);
+        }
+    }
+}
